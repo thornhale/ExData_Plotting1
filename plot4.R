4595d20e41ec509fad299dbf2d19565698890ddc
thornhale <thornhale@live.com>
2015-03-08 16:36:59 -0700

    REF: Updating plot4 script to produce 3 plots.

--- plot4.R
+++ plot4.R
@@ -7,26 +7,23 @@ plot4 <- function(fileLocation){
   powerData$DateTime <- strptime(paste(powerData$Date, powerData$Time), format='%Y-%m-%d %H:%M:%S')
   subPowerData <- powerData[(powerData$Date == as.Date('2007-02-01') | powerData$Date == as.Date('2007-02-02')),]
   png(filename = './figure/plot4.png', width=480, height=480)
-  par(mfrow=c(2,2))
-  with(
-    subPowerData,
-    plot(subPowerData$DateTime, subPowerData$Global_active_power, type='l', xlab='', ylab='Global Active Power (kilowatts)'),
-    plot(subPowerData$DateTime, subPowerData$Voltage, type='l', xlab='datetime', ylab='Voltage'),
-    plot(subPowerData$DateTime, subPowerData$Sub_metering_1, type='l', xlab='', ylab='Energy sub metering'),
-    lines(subPowerData$DateTime, subPowerData$Sub_metering_2, type = "l", col='red'),
-    lines(subPowerData$DateTime, subPowerData$Sub_metering_3, type = "l", col='blue'),
-    legend("topright", c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), col=c('black', 'red', 'blue')),
-    plot(subPowerData$DateTime, subPowerData$Global_reactive_power, type='l', xlab='datetime', ylab='Global_reactive_power')
-  )
+  par(mfrow = c(2,2))
+  with(subPowerDataFS, {
+    plot(subPowerData$DateTime, subPowerData$Global_active_power, 
+         type='l', ylab='Global Active Power', xlab='')
+    plot(subPowerData$DateTime, subPowerData$Voltage, type='l', 
+         ylab='Voltage', xlab='datetime')
+    plot(subPowerData$DateTime, subPowerData$Global_reactive_power, type='l', 
+         ylab='Global_reactive_power', xlab='datetime')
+  })
   dev.off()
-  with(
-    subPowerData,
-    plot(subPowerData$DateTime, subPowerData$Global_active_power, type='l', xlab='', ylab='Global Active Power (kilowatts)'),
-    plot(subPowerData$DateTime, subPowerData$Voltage, type='l', xlab='datetime', ylab='Voltage'),
-    plot(subPowerData$DateTime, subPowerData$Sub_metering_1, type='l', xlab='', ylab='Energy sub metering'),
-    lines(subPowerData$DateTime, subPowerData$Sub_metering_2, type = "l", col='red'),
-    lines(subPowerData$DateTime, subPowerData$Sub_metering_3, type = "l", col='blue'),
-    legend("topright", c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), col=c('black', 'red', 'blue')),
-    plot(subPowerData$DateTime, subPowerData$Global_reactive_power, type='l', xlab='datetime', ylab='Global_reactive_power')
-  )
+  par(mfrow = c(2,2))
+  with(subPowerDataFS, {
+    plot(subPowerData$DateTime, subPowerData$Global_active_power, 
+         type='l', ylab='Global Active Power', xlab='')
+    plot(subPowerData$DateTime, subPowerData$Voltage, type='l', 
+         ylab='Voltage', xlab='datetime')
+    plot(subPowerData$DateTime, subPowerData$Global_reactive_power, type='l', 
+         ylab='Global_reactive_power', xlab='datetime')
+})
 }
