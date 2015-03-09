81bc4ffe0fe80e888d2b63799536d4437ee2282b
thornhale <thornhale@live.com>
2015-03-08 21:55:42 -0700

    REF: Modified typos in plot4.R to produce plot4.

--- plot4.R
+++ plot4.R
@@ -13,6 +13,10 @@ plot4 <- function(fileLocation){
          type='l', ylab='Global Active Power', xlab='')
     plot(subPowerData$DateTime, subPowerData$Voltage, type='l', 
          ylab='Voltage', xlab='datetime')
+    plot(subPowerData$DateTime, subPowerData$Sub_metering_1, type='l', xlab='', ylab='Energy sub metering')
+    lines(subPowerData$DateTime, subPowerData$Sub_metering_2, type = "l", col='red')
+    lines(subPowerData$DateTime, subPowerData$Sub_metering_3, type = "l", col='blue')
+    legend("topright", c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), col=c('black', 'red', 'blue'))
     plot(subPowerData$DateTime, subPowerData$Global_reactive_power, type='l', 
          ylab='Global_reactive_power', xlab='datetime')
   })
@@ -23,7 +27,11 @@ plot4 <- function(fileLocation){
          type='l', ylab='Global Active Power', xlab='')
     plot(subPowerData$DateTime, subPowerData$Voltage, type='l', 
          ylab='Voltage', xlab='datetime')
+    plot(subPowerData$DateTime, subPowerData$Sub_metering_1, type='l', xlab='', ylab='Energy sub metering')
+    lines(subPowerData$DateTime, subPowerData$Sub_metering_2, type = "l", col='red')
+    lines(subPowerData$DateTime, subPowerData$Sub_metering_3, type = "l", col='blue')
+    legend("topright", c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), col=c('black', 'red', 'blue'))
     plot(subPowerData$DateTime, subPowerData$Global_reactive_power, type='l', 
          ylab='Global_reactive_power', xlab='datetime')
-})
+  })
 }
